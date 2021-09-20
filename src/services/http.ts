495cd8885528b1ec89dbdc9381512404a38171b5
Chris <gundamx9740@gmail.com>
2021-09-20 17:56:12 -0500

    Abstract API key to secrets file

--- src/services/http.ts
+++ src/services/http.ts
@@ -1,13 +1,14 @@
 // HTTP Service
 // OpenWeather: https://home.openweathermap.org/api_keys
 
+import { SECRETS } from './../../secrets';
+
 export class HttpService {
     private baseUrl: string = 'api.openweathermap.org/data/2.5';
-    private apiKey: string = 'ae16ffe6d0c251d75cafc95e21283fe5';
     private units: string = 'imperial';
 
     get(endpoint: string, cityName: string): Promise<Response> {
-        const url: string = `http://${this.baseUrl}${endpoint}?q=${cityName}&appid=${this.apiKey}&units=${this.units}`;
+        const url: string = `http://${this.baseUrl}${endpoint}?q=${cityName}&appid=${SECRETS.openWeatherAPI}&units=${this.units}`;
         return fetch(url, { method: 'GET' }).then((r) => r.json());
     }
 }
